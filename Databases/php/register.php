9bfeef864cbad8551b1f4fc0053d2b2692849a17
jarestides <75347199+jarestides@users.noreply.github.com>
2023-04-15 23:18:43 -0400

    Update register.php

--- Databases/php/register.php
+++ Databases/php/register.php
@@ -3,7 +3,7 @@
 $invalid='';
 
 if(isset($_POST['submit'])){
-    if(empty($_POST['fname']) || empty($_POST['lname']) || empty($_POST['user_id']) || empty($_POST['password']) || empty($_POST['email'])){
+    if(empty($_POST['fname']) || empty($_POST['lname']) || empty($_POST['user_id']) || empty($_POST['password']) || empty($_POST['email']) || empty($_POST['univ_id'])){
         $invalid ="Must fill the areas completely";
     }
     else
@@ -16,8 +16,9 @@ if(isset($_POST['submit'])){
         $user_id=$_POST['user_id'];
         $password=$_POST['password'];
         $email=$_POST['email'];
+        $univ_id=$_POST['univ_id'];
 
-        $register_query = "INSERT INTO `users` (`fname`, `lname`, `user_id`, `password`, `email`) VALUES ('$fname', '$lname', '$user_id', '$password', '$email')";
+        $register_query = "INSERT INTO `users` (`fname`, `lname`, `user_id`, `password`, `email`, `univ_id`) VALUES ('$fname', '$lname', '$user_id', '$password', '$email', '$univ_id')";
 
     try{
         $register_result = mysqli_query($conn, $register_query);
