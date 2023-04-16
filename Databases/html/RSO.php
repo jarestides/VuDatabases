d86725a750c1b1c493b5fd94ac723e53c7a3bf1b
jarestides <75347199+jarestides@users.noreply.github.com>
2023-04-16 01:45:31 -0400

    Add files via upload

---
+++ Databases/html/RSO.php
@@ -0,0 +1,63 @@
+<?php
+  include '../php/createRSO.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Login Form</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" type="text/css" href="../css/newLogin_style.css" />
+</head>
+
+<script> 
+ 
+  // Check to see if login info is valid 
+  function validateRSO() { 
+    var status = "none"; 
+    var RSOError = document.getElementById("RSOError"); 
+ 
+    status = "<%= request.getAttribute("status") %>"; 
+    if (status == "fail") { 
+        console.log("Testing RSO status fail"); 
+ 
+        RSOError.textContent = "RSO is incorrect"; 
+        RSOError.style.color = "red"; 
+        console.log("Testing RSO error check"); 
+      return false; 
+    } 
+ 
+  } 
+   
+  validateRSO(); 
+ 
+</script>
+
+<body>
+  <div class="limiter">
+    <div class="container-login">
+      <div class="title">
+        <h1>Events</h1>
+        <div class="wrap-login">
+          <form class="login-form validate-form" method="post" action="">
+            <span class="login-form-title"> <b>Create RSO</b></span>
+            <input class="input" type="text" name="Name" id="Name" placeholder="Name" />
+            <input class="input" type="text" name="Description" id="Description" placeholder="Description" style = "height: 100px;" />
+            <button class="login-form-btn" type="submit" name="submit">
+              Create
+            </button>
+            <div class="login-form-create">
+            <a href="login-access.php">Go Back</a>
+            <span id="loginError"></span>
+          </div>
+            <span><?php echo $invalid; ?></span>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
