e5648d92808ccb2b91ff45709e6c45017e1e5cb7
jarestides <75347199+jarestides@users.noreply.github.com>
2023-04-16 01:45:03 -0400

    Update login-access.php

--- Databases/html/login-access.php
+++ Databases/html/login-access.php
@@ -6,7 +6,7 @@
 <html lang="en">
 
 <head>
-  <title>Login Form</title>
+  <title>Dashboard</title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="stylesheet" type="text/css" href="../css/newLogin_style.css" />
@@ -42,12 +42,11 @@
         <h1>Events</h1>
         <div class="wrap-login">
           <form class="login-form validate-form" method="post" action="">
-            <span class="login-form-title"> <b>Create RSO</b></span>
-            <input class="input" type="text" name="Name" id="Name" placeholder="Name" />
-            <input class="input" type="text" name="Description" id="Description" placeholder="Description" style = "height: 100px;" />
-            <button class="login-form-btn" type="submit" name="submit">
-              Create
-            </button>
+            <a class="login-form-btn" style="text-decoration:none" href="RSO.php">Go to RSO</a>
+            <a class="login-form-btn" style="text-decoration:none" href="Events.php">Go to Events</a>
+          <div class="login-form-create">
+            <a href="loginPage.php">Go Back</a>
+          </div>
             <span><?php echo $invalid; ?></span>
           </form>
         </div>
