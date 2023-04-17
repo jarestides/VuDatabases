dcc0d6ef8c72be12b9be35e355d204fca745abec
tkidd9 <106347455+tkidd9@users.noreply.github.com>
2023-04-17 04:02:31 -0400

    Add files via upload

--- Databases/php/createRSO.php
+++ Databases/php/createRSO.php
@@ -25,19 +25,20 @@ if(isset($_POST['submit'])){
         
         $query = mysqli_query($conn, "INSERT INTO `rso` (name, Description, users_user_id, univprofile_univ_id) VALUES ('$name', '$Description', '$users_user_id', '$univprofile_univ_id')");
 
-        $rows = mysqli_num_rows($query);
+        //$rows = mysqli_num_rows($query);
+        
         
 //        $result = mysqli_result($query);
 //        $row = mysqli_fetch_fields($query);
 //        $user_type = 2;
 
-        if($rows == 1){
-            //$query2 = mysqli_query($conn, "UPDATE users SET user_type='$user_type' WHERE user_id='$users_user_id'");
-            //returnWithInfo($row['name']);
-            header("Location: login-access.php");
-        }else{
-            echo("Invalid");
-        }
+       // if($rows == 1){
+//            $query2 = mysqli_query($conn, "UPDATE users SET user_type='$user_type' WHERE user_id='$users_user_id'");
+//            returnWithInfo($row['name']);
+        header("Location: login-access.php");
+//        }else{
+//            echo("Invalid");
+//        }
         mysqli_close($conn);
     }
 }
