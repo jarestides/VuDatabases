cae904f05ec821afb5677a5435362888bb11c9d4
tkidd9 <106347455+tkidd9@users.noreply.github.com>
2023-04-17 23:42:09 -0400

    Add files via upload

--- Databases/php/hostEvent.php
+++ Databases/php/hostEvent.php
@@ -18,33 +18,34 @@ if(isset($_POST['submit'])){
         $conn = mysqli_connect("localhost", "root", "");
         $db = mysqli_select_db($conn, "vutabase");
         
-        $users_user_id = $_POST["users_user_id"];
-        $rso_RSO_id = $_POST["rso_RSO_id"];
-        $name=$_POST['name'];
-        $category=$_POST['category'];
-        $description=$_POST['description'];
-        $time=$_POST['time'];
-        $date=$_POST['date'];
-        $location=$_POST['location'];
-        $phone=$_POST['phone'];
-        $email=$_POST['email'];
+        $name = $_POST['name'];
+        $category = $_POST['category'];
+        $description = $_POST['description'];
+        $time = $_POST['time'];
+        $date = $_POST['date'];
+        $location = $_POST['location'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $univprofile_univ_id = $_POST['univprofile_univ_id'];
         
-        $query = mysqli_query($conn, "INSERT INTO `events` (`name`, `category`, `description`, `time`, `date`, `location`, `phone`, `email`) VALUES ('$name', '$category', '$description', '$time', '$date', '$location', '$phone', '$email')");
+        $query = mysqli_query($conn, "INSERT INTO `events` (`name`, `description`, `time`, `date`, `location`, `rating`, `univprofile_univ_id`, `rso_RSO_id`, `users_user_id`, `phone`, `email`, `category`) VALUES ('$name', '$description', '$time', '$date', '$location', NULL, '$univprofile_univ_id', NULL, NULL, '$phone', '$email', '$category')");
+        
+ 
 
-        //$rows = mysqli_num_rows($query);
+        $rows = mysqli_num_rows($query);
         
         
 //        $result = mysqli_result($query);
 //        $row = mysqli_fetch_fields($query);
 //        $user_type = 2;
 
-       // if($rows == 1){
+        if($rows == 1){
 //            $query2 = mysqli_query($conn, "UPDATE users SET user_type='$user_type' WHERE user_id='$users_user_id'");
 //            returnWithInfo($row['name']);
-        header("Location: login-access.php");
-//        }else{
-//            echo("Invalid");
-//        }
+            header("Location: login-access.php");
+        }else{
+            echo("Invalid");
+        }
         mysqli_close($conn);
     }
 }
