24ad136253aee8da0a6265b5b1a3b489a06c742d
tkidd9 <106347455+tkidd9@users.noreply.github.com>
2023-04-17 21:53:18 -0400

    Add files via upload

---
+++ Databases/php/joinRSO.php
@@ -0,0 +1,36 @@
+<?php
+
+$invalid='';
+
+if(isset($_POST['submit'])){
+    if(empty($_POST['users_user_id']) || empty($_POST['rso_RSO_id'])){
+        $invalid ="Must fill the areas completely";
+    }
+    else
+    {
+        $conn = mysqli_connect("localhost", "root", "");
+        $db = mysqli_select_db($conn, "vutabase");
+        
+        $users_user_id = $_POST['users_user_id'];
+        $rso_RSO_id = $_POST['rso_RSO_id'];
+        
+        $query = mysqli_query($conn, "INSERT INTO `rso_members` (users_user_id, rso_RSO_id) VALUES ('$users_user_id', '$rso_RSO_id')");
+
+        $rows = mysqli_num_rows($query);
+        
+//        $result = mysqli_result($query);
+//        $row = mysqli_fetch_fields($query);
+//        $user_type = 2;
+
+//        if($rows == 1){
+            //$query2 = mysqli_query($conn, "UPDATE users SET user_type='$user_type' WHERE user_id='$users_user_id'");
+            //returnWithInfo($row['name']);
+        header("Location: login-access.php");
+//        }else{
+//            echo("Invalid");
+//        }
+        mysqli_close($conn);
+    }
+}
+
+?>
